916e83d3ddc08d639ec1fffe016ac4fe2ed4cba4
Benjamin Downs <benjamin.downs@gmail.com>
2016-04-24 21:46:51 -0500

    getting routes in order. got ride of typescript for now

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 "use strict";
 var express = require('express');
-var router = express.Router();
-var app = express();
+var router  = express.Router();
+var app     = express();
+var port    =  8080;
+var path    = require('path')
+
 router.get('/', function (req, res) {
-    res.send('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+router.get('/api', function(req, res) {
+    res.send('hello from the api route')
+})
+
+router.get('/*', function(req,res) {
+    res.send('hello from the wildcard')
+})
+
+app.use('/', router)
+
+app.listen(port);
+console.log(`listening on port ${port}`)
